3b3f2cbedd6d79c0c8540cf403e8fe4243ab5cb1
Udit Saxena <40498341+udit56@users.noreply.github.com>
2018-12-28 12:48:12 +0530

    Add files via upload

---
+++ player/reverse_string_no_vowels.py
@@ -0,0 +1,13 @@
+def reverse(s):
+    li=['a','e','i','o','u']
+    for i in range(len(s)-1,-1,-1):
+        if s[i] in li:
+            pass
+        else:
+            print(s[i],end="")
+
+try:
+    s=input()
+    reverse(s)
+except ValueError:
+    print("Invalid input")
