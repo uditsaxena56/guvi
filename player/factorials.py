07e48dd86931ba58b76a683876c33e6d1718ddac
Udit Saxena <40498341+udit56@users.noreply.github.com>
2018-12-28 11:59:34 +0530

    Update factorials.py

--- player/factorials.py
+++ player/factorials.py
@@ -1,11 +1,15 @@
 def factorial(n):
-    s=1
-    for i in range(1,n+1):
-        s=s*i
-    print(s)
+    if n >20:
+        print("Size too big")
+    else:
+        s=1
+        for i in range(1,n+1):
+            s=s*i
+        return s
 
 try:
     n=int(input())
-    factorial(n)
+    s=factorial(n)
+    print(s)
 except ValueError:
     print("Invalid input")
